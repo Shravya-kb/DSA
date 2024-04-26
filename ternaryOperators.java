8093631cfb366cf29e35fb36838571314baf567d
Shravya-kb <123442379+Shravya-kb@users.noreply.github.com>
2024-04-26 14:35:30 +0530

    Create ternaryOperators.java
    To find whether a given number in the code is a even or not using ternary operator

---
+++ ternaryOperators.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ternaryOperator {
+  public static void main(String args[]){
+    int number = 4;
+
+    //ternary operator
+    String type = ((number%2) == 0) ? "even" : "odd";
+    System.out.println(type);
+  }
+}
+//output:
+//even
